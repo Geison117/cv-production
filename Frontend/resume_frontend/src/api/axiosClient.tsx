1684ae8fe732c4ee287a99692051f0356a20a990
Geison Blanco <geisonblanco94@gmail.com>
2026-02-16 11:10:24 -0500

    Update axiosClient.tsx

--- Frontend/resume_frontend/src/api/axiosClient.tsx
+++ Frontend/resume_frontend/src/api/axiosClient.tsx
@@ -22,6 +22,7 @@ export default api;
 
 // fetchClient.ts
 const BASE_URL = "https://hoja-de-vida-full-stack-ewp4.vercel.app"; // tu backend en vercel
+const cont = 1;
 
 export async function fetchClient(
   endpoint: string,
