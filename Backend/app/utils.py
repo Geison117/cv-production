4c41c7a611291ab89fe1a663afda295dffe2e6b5
Geison Blanco <geisonblanco94@gmail.com>
2026-02-22 22:53:02 -0500

    Update utils.py

--- Backend/app/utils.py
+++ Backend/app/utils.py
@@ -1,12 +1,17 @@
+from select import select
+
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from sqlalchemy import select
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from .database import get_db
 from . import models
 from .config import settings
+
 
 SECRET_KEY = settings.SECRET_KEY #"super_secret_key_cambiar_en_produccion"
 ALGORITHM = "HS256"
@@ -32,9 +37,9 @@ def verify_password(plain_password, hashed_password):
 
 
 
-def get_current_user(
+async def get_current_user(
     token: str = Depends(oauth2_scheme),
-    db: Session = Depends(get_db)
+    db: AsyncSession = Depends(get_db)
 ):
 
     credentials_exception = HTTPException(
@@ -53,9 +58,11 @@ def get_current_user(
     except JWTError:
         raise credentials_exception
 
-    user = db.query(models.Usuario).filter(
-        models.Usuario.id == user_id
-    ).first()
+    result = await db.execute(
+        select(models.Usuario).where(models.Usuario.id == user_id)
+    )
+    
+    user = result.scalar_one_or_none()
 
     if user is None:
         raise credentials_exception
